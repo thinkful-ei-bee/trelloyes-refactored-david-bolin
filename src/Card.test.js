6b8065d364a9011df57f942a4ba2eff96ce7ff8b
David Bolin <david@bolin.at>
2019-03-23 13:16:00 -0700

    smoke and snapshot tests

--- src/Card.test.js
+++ src/Card.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Card from './Card.js';
+import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Card title="x" content="z"/>, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('Card Component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Card title="x" content="z"/>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders the UI as expected', () => {
+    const tree = renderer
+      .create(<Card title="First Card" content="lorem ipsum" />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();  
+    });
 });
--- src/List.test.js
+++ src/List.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import List from './Card.js';
+import List from './List.js';
+import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<List header="x" cards={[1, 2]}/>, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('List Component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<List header="x" cards={[1, 2]}/>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  
+  it('renders the UI as expected', () => {
+    const tree = renderer
+      .create(<List header="First List" cards={['a', 'b']} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();  
+  });
 });
